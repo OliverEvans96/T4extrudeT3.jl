27086e73e39ecc8140f9d04d59e87b5cb093462b
Oliver Evans <oliverevans96@gmail.com>
2021-02-14 01:27:31 -0500

    Clean up

--- src/finetools_graph.jl
+++ src/finetools_graph.jl
@@ -1,65 +1,23 @@
 module finetools_graph
 
-using FinEtools
-using SymRCM: adjgraph
 using IterTools
 
 using JuMP
 using Cbc
 
-export create_t3_mesh
-function create_t3_mesh()
-     # Start from tetrahedral mesh
-    xmin, xmax = -6.0, 6.0
-    ymin, ymax = -2.5, 2.5
-    lf = xmax - xmin
-    wf = ymax - ymin
-    nxf = 4
-    nyf = 5
-    t4nodes, t4elems = T4block(lf, wf, 1.0, nxf, nyf, 1)
-    t4nodes.xyz[:,1] .+= xmin
-    t4nodes.xyz[:,2] .+= ymin
-    # Select boundary
-    t3elems = meshboundary(t4elems)
-    # Exclude downward-ish faces (the floor)
-    bottom_inds = selectelem(t4nodes, t3elems, facing=true, direction=[0.0, 0.0, -1.0])
-    t3elems = subset(t3elems, bottom_inds)
-    # Create new node set with only selected nodes
-    connected = findunconnnodes(t4nodes, t3elems);
-    t3nodes, new_numbering = compactnodes(t4nodes, connected);
-    renumberconn!(t3elems, new_numbering);
-    # Modify original mesh a bit
-    f((x, y, z)) = [x y 0.4*(sin(2*x) + cos(2*y))]
-    #t3nodes.xyz .= reduce(vcat, f.(eachrow(t3nodes.xyz)))
-
-    return t3nodes, t3elems
-end
-
-export adjgraph
-function adjgraph(nodes::FENodeSet, elems::AbstractFESet2Manifold)
-    num_nodes = count(nodes)
-    conn = connasarray(elems)
-    return adjgraph(conn, num_nodes)
-end
-
-function isconnected(el1::T, el2::T) where T <: NTuple{3, FInt}
+function isconnected(el1::T, el2::T) where T <: NTuple{3, Int}
     # Two triangles share an edge if they have two nodes in common
     length(el1 ∩ el2) == 2
 end
 
-export dualadj
-function dualadj(elems::AbstractFESet2Manifold)
-    """
-    Construct the dual adjacency graph for a triangular mesh.
-    In other words, determine which elements share an edge.
-    """
+export get_edge_info
+function get_edge_info(elems::AbstractFESet2Manifold)
 
     # Index i = element number (index of `elems` and `elem_edges` (outer vector))
     # Index j = element-relative edge number (index of `elem_edges` (inner vectors))
     # Index k = global edge number (index of `edge_elems` and `edge_nodes`)
 
     nelems = count(elems)
-    #elemadj::Vector{Vector{Int}} = fill([], nelems)
     elem_edges::Vector{Vector{Int}} = fill([], nelems)
     edge_elems::Vector{NTuple{2, NTuple{2, Int}}} = []
     edge_nodes::Vector{NTuple{2, Int}} = []
@@ -70,10 +28,6 @@ function dualadj(elems::AbstractFESet2Manifold)
         el2 = elems.conn[i2]
         if isconnected(el1, el2)
             k += 1
-
-            # Elems are adjacent
-            # el1_edge_ind = push!(elemadj[i1], i2) |> length
-            # el2_edge_ind = push!(elemadj[i2], i1) |> length
 
             # Elems share the edge
             j1 = push!(elem_edges[i1], k) |> length
@@ -88,9 +42,6 @@ function dualadj(elems::AbstractFESet2Manifold)
             # Store the nodes connected by the edge
             n1_ind, n2_ind = elems.conn[i1] ∩ elems.conn[i2]
             push!(edge_nodes, (n1_ind, n2_ind))
-
-            # # Save the index of this edge in each elem's list
-            # push!()
         end
     end
     return edge_elems, edge_nodes, elem_edges
@@ -126,6 +77,7 @@ function orient_edges(nelems, edge_elems, optimizer=Cbc.Optimizer)
     optimize!(model)
     return convert(BitArray, round.(Int, value.(edge_orients)))
 end
+
 
 export orient_elems
 function orient_elems(edge_orients, edge_nodes, elem_nodes, elem_edges)
@@ -165,12 +117,6 @@ function orient_elems(edge_orients, edge_nodes, elem_nodes, elem_edges)
 
         # The start node is opposite this edge ("center edge")
         j = findfirst(≠(elem_variant), elem_edge_orients)
-
-        println()
-        @show elem_variant
-        @show elem_edge_orients
-        @show j
-        @show nadj
 
         if j <= nadj
             # The center edge is a known edge
@@ -222,13 +168,9 @@ function orient_elems(edge_orients, edge_nodes, elem_nodes, elem_edges)
                 push!(elem_orients, elem_orient)
             end
         end
-        @show elem_orients[i]
     end
 
     return elem_variants, elem_orients
 end
-
-export greet
-greet() = print("Hello World!")
 
 end # module
--- src/test/runtests.jl
+++ src/test/runtests.jl
@@ -1,10 +1,36 @@
 using finetools_graph
+using FinEtools
 
+function create_t3_mesh()
+     # Start from tetrahedral mesh
+    xmin, xmax = -6.0, 6.0
+    ymin, ymax = -2.5, 2.5
+    lf = xmax - xmin
+    wf = ymax - ymin
+    nxf = 4
+    nyf = 5
+    t4nodes, t4elems = T4block(lf, wf, 1.0, nxf, nyf, 1)
+    t4nodes.xyz[:,1] .+= xmin
+    t4nodes.xyz[:,2] .+= ymin
+    # Select boundary
+    t3elems = meshboundary(t4elems)
+    # Exclude downward-ish faces (the floor)
+    bottom_inds = selectelem(t4nodes, t3elems, facing=true, direction=[0.0, 0.0, -1.0])
+    t3elems = subset(t3elems, bottom_inds)
+    # Create new node set with only selected nodes
+    connected = findunconnnodes(t4nodes, t3elems);
+    t3nodes, new_numbering = compactnodes(t4nodes, connected);
+    renumberconn!(t3elems, new_numbering);
+    # Modify original mesh a bit
+    f((x, y, z)) = [x y 0.4*(sin(2*x) + cos(2*y))]
+    #t3nodes.xyz .= reduce(vcat, f.(eachrow(t3nodes.xyz)))
+
+    return t3nodes, t3elems
+end
 
 nodes, elems = create_t3_mesh()
-nodeadj = adjgraph(nodes, elems)
 elem_nodes = elems.conn
-edge_elems, edge_nodes, elem_edges = dualadj(elems)
+edge_elems, edge_nodes, elem_edges = get_edge_info(elems)
 
 edge_orients = orient_edges(count(elems), edge_elems)
 elem_variants, elem_orients = orient_elems(edge_orients, edge_nodes, elem_nodes, elem_edges)
